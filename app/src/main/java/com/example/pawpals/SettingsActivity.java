083205d4268a5ece9587ab5953dd48476dde1b5d
testmac <adiwayn@gmail.com>
2025-06-04 12:59:13 +0300

    04.06.25

--- app/src/main/java/com/example/pawpals/SettingsActivity.java
+++ app/src/main/java/com/example/pawpals/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.example.pawpals;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -8,5 +11,13 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button backButton = findViewById(R.id.button_back);
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            startActivity(intent);
+            finish();
+        });
     }
 }
