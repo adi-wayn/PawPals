0e4489852a65ad2124b5fd380f036f2d073955db
testmac <adiwayn@gmail.com>
2025-06-12 15:42:32 +0300

    12.06.25

--- app/src/main/java/com/example/pawpals/CommunityActivity.java
+++ app/src/main/java/com/example/pawpals/CommunityActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.widget.Button;
-
 import model.User;
 
 public class CommunityActivity extends AppCompatActivity {
