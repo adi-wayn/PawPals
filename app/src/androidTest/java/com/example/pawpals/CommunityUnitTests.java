5bfec66035b703503073f9f679c98b932d02927a
Eyad1803 <e-abo@hotmail.com>
2025-06-28 00:13:48 +0300

    Add files via upload

---
+++ app/src/androidTest/java/com/example/pawpals/CommunityUnitTests.java
@@ -0,0 +1,60 @@
+package com.example.pawpals;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import model.*;
+
+public class CommunityUnitTests {
+
+    private Community community;
+    private CommunityManager manager;
+
+    @Before
+    public void setUp() {
+        manager = new CommunityManager("Dor", "ben-devide");
+        community = new Community("ben-devide", manager);
+    }
+
+    @Test
+    public void testCommunityName() {
+        assertEquals("ben-devide", community.getName());
+    }
+
+    @Test
+    public void testManagerAssignment() {
+        assertEquals(manager, community.getManager());
+        assertTrue(manager.isManager());
+    }
+
+    @Test
+    public void testAddAndGetReports() {
+        Report r1 = new Report("post", "ALEX", "Lost Dog", "Please help me find my dog.");
+        Report r2 = new Report("complaint", "Sara", "Noise", "Too much barking.");
+
+        community.addReport(r1);
+        community.addReport(r2);
+
+        ArrayList<Report> allReports = community.getReports();
+        assertEquals(2, allReports.size());
+
+        ArrayList<Report> posts = community.getReportByType("post");
+        assertEquals(1, posts.size());
+        assertEquals("ALEX", posts.get(0).getSenderName());
+    }
+
+    @Test
+    public void testAddMembers() {
+        User user1 = new User("Ady", "ben-devide");
+        ArrayList<User> newMembers = new ArrayList<>();
+        newMembers.add(user1);
+
+        community.setMembers(newMembers);
+
+        assertEquals(1, community.getMembers().size());
+        assertEquals("Ady", community.getMembers().get(0).getUserName());
+    }
+}
