abf6a953fa4736192b5b2d82a69d347ed8526b2d
testmac <adiwayn@gmail.com>
2025-08-18 00:07:02 +0300

    סידור המסך הראשי חלון צד

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,5 @@ dependencies {
      testImplementation ("org.mockito:mockito-core:5.11.0")
     testImplementation ("org.mockito.kotlin:mockito-kotlin:5.1.0")
     testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
